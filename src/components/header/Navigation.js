d8f9a13373621ab6c3ec810d69d1ef894a92fcb1
Juraj Urban <durko11169@gmail.com>
2022-03-07 12:48:36 +0100

    add logout function and use authContext

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,21 +1,36 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 import Button from "../UI/button/Button";
 import styles from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const authContext = useContext(AuthContext);
+
+  const isLoggedIn = authContext.isLoggedIn;
+
+  const logoutHandler = () => {
+    authContext.logout();
+  };
+
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
-          <Link to="/login">
-            <Button title="Sign in" />
-          </Link>
-        </li>
-        <li>
-          <Link to="/">
-            <Button title="Logout" />
-          </Link>
-        </li>
+        {!isLoggedIn && (
+          <li>
+            <Link to="/login">
+              <Button title="Sign in" />
+            </Link>
+          </li>
+        )}
+
+        {isLoggedIn && (
+          <li>
+            <Link to="/">
+              <Button title="Logout" onClick={logoutHandler} />
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
