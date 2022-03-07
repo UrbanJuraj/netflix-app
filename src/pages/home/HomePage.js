d5ab0a20d95ecc1fe68a0e45d3ba0ed109d66cde
Juraj Urban <durko11169@gmail.com>
2022-03-07 15:26:54 +0100

    fetch data and display to carousel

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 
-import styles from "./HomePage.module.css";
-import img from "../../assets/introduction-img.jpg";
 import SearchForm from "../../components/home/SearchForm";
 import Card from "../../components/UI/Card";
+import notFoundImg from "../../assets/not-found.png";
+
+import styles from "./HomePage.module.css";
 
 const HomePage = (props) => {
+  const [movies, setMovies] = useState([]);
+
+  const randomPageNumber = Math.floor(Math.random() * 100) + 1;
+
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=8ef9ad71a27968a7f2c8623c3cd32d1a&language=en-US&page=${randomPageNumber}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const resData = await response.json();
+
+      let loadedMovies = [];
+
+      resData.results.map((movie) =>
+        loadedMovies.push({
+          key: movie.id,
+          id: movie.id,
+          title: movie.title,
+          image: movie.backdrop_path,
+        })
+      );
+
+      setMovies(loadedMovies);
+    };
+
+    fetchData();
+  }, []);
+
+  const imgHttp = `https://image.tmdb.org/t/p/original`;
+
   return (
     <Card>
       <div className={styles.gap}>
@@ -13,26 +45,17 @@ const HomePage = (props) => {
 
         <div className={styles.carousel}>
           <Carousel>
-            <Carousel.Item>
-              <img src={img} alt="wedw" />
-              <Carousel.Caption>
-                <p>carousel</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <img src={img} alt="wdwd" />
-              <Carousel.Caption>
-                <p>carousel</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-              <img src={img} alt="wedw" />
-              <Carousel.Caption>
-                <p>carousel</p>
-              </Carousel.Caption>
-            </Carousel.Item>
+            {movies.map((movie) => (
+              <Carousel.Item key={movie.key}>
+                <img
+                  src={movie.image ? imgHttp + movie.image : notFoundImg}
+                  alt="movie img"
+                />
+                <Carousel.Caption>
+                  <h1>{movie.title}</h1>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
           </Carousel>
         </div>
       </div>
