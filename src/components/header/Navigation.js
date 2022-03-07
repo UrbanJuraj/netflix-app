36f684902b31d51fe3e002f57081b141b234e08d
Juraj Urban <durko11169@gmail.com>
2022-03-08 00:35:07 +0100

    removing searched movies when user logout

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,16 +1,19 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
+import MoviesContext from "../../store/movies-context";
 import Button from "../UI/button/Button";
 import styles from "./Navigation.module.css";
 
 const Navigation = (props) => {
   const authContext = useContext(AuthContext);
+  const moviesContext = useContext(MoviesContext);
 
   const isLoggedIn = authContext.isLoggedIn;
 
   const logoutHandler = () => {
     authContext.logout();
+    moviesContext.clearMovies();
   };
 
   return (
