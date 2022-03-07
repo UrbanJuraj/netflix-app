04396513ef4d688b1e3f083807d0b4967fe4c81d
Juraj Urban <durko11169@gmail.com>
2022-03-07 12:46:52 +0100

    using authContext to navigate when click on netfli

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,19 @@
-import styles from "./Header.module.css";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import AuthContext from "../../store/auth-context";
 import Navigation from "./Navigation";
 
+import styles from "./Header.module.css";
+
 const Header = (props) => {
+  const authContext = useContext(AuthContext);
+
   return (
     <header className={styles["main-header"]}>
-      <h1>Netflix</h1>
+      <Link to={authContext.isLoggedIn ? "/home" : "/"}>
+        <h1>Netflix</h1>
+      </Link>
       <Navigation />
     </header>
   );
