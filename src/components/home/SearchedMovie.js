cff3da72b2aaf3d644f87870652ec6a92c2792ca
Juraj Urban <durko11169@gmail.com>
2022-03-08 00:23:59 +0100

    add component for searched movies

---
+++ src/components/home/SearchedMovie.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import MoviesContext from "../../store/movies-context";
+import styles from "./SearchedMovie.module.css";
+
+const SearchedMovie = (props) => {
+  const navigate = useNavigate();
+
+  const moviesContext = useContext(MoviesContext);
+
+  const onClickHandler = () => {
+    moviesContext.setMovie(props.movie);
+    navigate("/movie");
+  };
+
+  return (
+    <div className={styles["searched-movie"]} onClick={onClickHandler}>
+      <h2>{props.movie.title}</h2>
+    </div>
+  );
+};
+
+export default SearchedMovie;
