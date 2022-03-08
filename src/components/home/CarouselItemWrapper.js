c32eeb5509cd2b343c5db75eded0fabbdc452dae
Juraj Urban <durko11169@gmail.com>
2022-03-08 14:57:39 +0100

    add carousel wrapper for adding onClick function

---
+++ src/components/home/CarouselItemWrapper.js
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import MoviesContext from "../../store/movies-context";
+
+const CarouselItemWrapper = (props) => {
+  const navigate = useNavigate();
+
+  const moviesContext = useContext(MoviesContext);
+
+  const onClickHandler = () => {
+    moviesContext.setMovie(props.movie);
+    navigate("/movie");
+  };
+
+  return <div onClick={onClickHandler}>{props.children}</div>;
+};
+
+export default CarouselItemWrapper;
