92df4cb91b02df7bf7e53c8d7b1076d3c140ac0e
Juraj Urban <durko11169@gmail.com>
2022-03-08 13:49:28 +0100

    handling errors in search form

--- src/components/home/SearchForm.js
+++ src/components/home/SearchForm.js
@@ -1,10 +1,12 @@
-import { useContext, useRef } from "react";
+import { Fragment, useContext, useRef, useState } from "react";
 import MoviesContext from "../../store/movies-context";
 import Button from "../UI/button/Button";
 import Input from "../UI/input/Input";
 import styles from "./SearchForm.module.css";
 
 const SearchForm = (props) => {
+  const [error, setError] = useState();
+
   const searchInputRef = useRef();
 
   const moviesContext = useContext(MoviesContext);
@@ -12,24 +14,44 @@ const SearchForm = (props) => {
   const submitFormHandler = (event) => {
     event.preventDefault();
 
+    setError();
+
     const enteredMovieName = searchInputRef.current.value;
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=8ef9ad71a27968a7f2c8623c3cd32d1a&language=en-US&query=${enteredMovieName}`;
+    if (enteredMovieName.length > 2) {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=8ef9ad71a27968a7f2c8623c3cd32d1a&language=en-US&query=${enteredMovieName}`;
 
-    const fetchData = async () => {
-      const response = await fetch(url);
-      const resData = await response.json();
-      moviesContext.addMovies(resData.results);
-    };
+      const fetchData = async () => {
+        const response = await fetch(url);
 
-    fetchData();
+        if (!response.ok) throw new Error("Something went wrong!");
+
+        const resData = await response.json();
+        moviesContext.addMovies(resData.results);
+      };
+
+      fetchData().catch((error) => {
+        setError(error.message);
+      });
+    } else {
+      moviesContext.clearMovies();
+      setError("Please write at least 3 letters!");
+    }
   };
 
   return (
-    <form onSubmit={submitFormHandler} className={styles.form}>
-      <Input type="text" placeholder="Search film" reference={searchInputRef} />
-      <Button title="Search" type="submit" />
-    </form>
+    <Fragment>
+      <form onSubmit={submitFormHandler} className={styles.form}>
+        <Input
+          type="text"
+          placeholder="Search film"
+          reference={searchInputRef}
+        />
+        <Button title="Search" type="submit" />
+      </form>
+
+      {error && <p className={styles.error}>{error}</p>}
+    </Fragment>
   );
 };
 
